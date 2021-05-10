9443096e4be139fa9de21e4abeb66f50e275faf6
Sae0524 <ume.m3s@gmail.com>
2021-05-10 10:26:46 -0700

    final

--- main.js
+++ main.js
@@ -15,22 +15,44 @@ const fetchAPI = async () => {
     //nowPlaying
     let movieImageURL = "http://image.tmdb.org/t/p/w500";
     const moviePlayingNow = movieData.results.map(movie => {
-      return `<li><div id ="${movie.id}" class="nowPlaying-box triggerMovie"><img src=${movieImageURL}${movie.backdrop_path}><p class="title">${movie.title}</p></div></li>`
+      return `<li>
+              <div id ="${movie.id}" class="nowPlaying-box triggerMovie">
+              <img src=${movieImageURL}${movie.backdrop_path}>
+              <p class="title">${movie.title}</p>
+              <button  class="triggerButton" onclick="on()" id=${movie.id}>See detail</button>
+              </div>
+              </div>
+              </li>`
     })
 
     //popular
     const moviePopular = moviePopularData.results.map(movie => {
-      return `<li><div class="popular-box triggerMovie"><img id ="poster" src=${movieImageURL}${movie.backdrop_path}><p>${movie.title}</p></div></li>`
+      return `<li>
+              <div id ="${movie.id}" class="popular-box triggerMovie">
+              <img src=${movieImageURL}${movie.backdrop_path}>
+              <p class="title">${movie.title}</p>
+              <button  class="triggerButton" onclick="on()" id=${movie.id}>See detail</button>
+              </div>
+              </div>
+              </li>`
     })
 
     //upooming
     const movieUpcoming = movieUpcomingData.results.map(movie => {
-      return `<li><div class="UpComing-box triggerMovie"><img id ="poster" src=${movieImageURL}${movie.backdrop_path}><p>${movie.title}</p></div></li>`
+      return `<li>
+              <div id ="${movie.id}" class="UpComing-box triggerMovie">
+              <img src=${movieImageURL}${movie.backdrop_path}>
+              <p class="title">${movie.title}</p>
+              <button  class="triggerButton" onclick="on()" id=${movie.id}>See detail</button>
+              </div>
+              </div>
+              </li>`
+
     })
 
-    document.getElementById('PlayingNow').innerHTML = moviePlayingNow;
-    document.getElementById('popular').innerHTML = moviePopular;
-    document.getElementById('UpComing').innerHTML = movieUpcoming;
+    document.getElementById('PlayingNow').innerHTML = moviePlayingNow.join('');
+    document.getElementById('popular').innerHTML = moviePopular.join('');
+    document.getElementById('UpComing').innerHTML = movieUpcoming.join('');
 
 
   }
@@ -43,21 +65,23 @@ fetchAPI(); //Call the function
 
 //fetch details data
 
-const fetchDetailData = async () => {
-  const divs = document.querySelectorAll('.a');
+const fetchDetailData = async (event) => {
+  let idMovie = event.target.id;
 
-  divs.forEach(el => el.addEventListener('click', event => {
-    console.log(event.target.getAttribute("data-el"));
-  }));
+  alert(idMovie);
+
 
   try {
-    const detailMovie = await axios.get(`https://api.themoviedb.org/3/movie/460465?api_key=${apikey}`);
+    const detailMovie = await axios.get(`https://api.themoviedb.org/3/movie/${idMovie}?api_key=${apikey}`);
     console.log(detailMovie);
+    console.log(trailar);
   }
   catch(errors) {
     console.log(`errors! ${errors}`);
   }
 }
+
+
 
 document.getElementsByClassName("clickem").addEventListener("click", function() {
   alert("hi")
